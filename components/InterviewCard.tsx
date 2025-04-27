72c7d5e35d8d6b78f1485472b723e38754843be7
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-26 22:51:35 -0700

    completed the pilot phase of the card

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,6 +1,8 @@
 import { getRandomInterviewCover } from '@/public/utils';
 import dayjs from 'dayjs';
 import Image from 'next/image';
+import { Button } from './ui/button';
+import Link from 'next/link';
 
 const InterviewCard = ({
   interviewId,
@@ -30,6 +32,46 @@ const InterviewCard = ({
             height={90}
             className="rounded-full object-fit size-[90px]"
           />
+
+          <h3 className="mt-5 capitalize">{role} Interview</h3>
+
+          <div className="flex flex-row gap-5 mt-3">
+            <div className="flex flex-row gap-2">
+              <Image
+                src="/calendar.svg"
+                alt="calendar"
+                width={22}
+                height={22}
+              />
+              <p>{formattedDate}</p>
+            </div>
+
+            <div className="flex flex-row gap-2 items-center">
+              <Image src="/star.svg" alt="star" width={22} height={22} />
+              <p>{feedback?.totalScore || '---'}/100</p>
+            </div>
+          </div>
+
+          <p className="line-clamp-2 mt-5">
+            {feedback?.finalAssessment ||
+              "You haven't taken the interview yet. Take it now to improve your skills."}
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between">
+          <p>Tech Icons</p>
+
+          <Button className="btn-primary">
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? 'Check Feedback' : 'View Interview'}
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
