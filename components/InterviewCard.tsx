966c0f9be1f06c7eeecc1202a3b8d6e1797c7dc4
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-27 22:21:40 -0700

    add pilot techstack icons in the card

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,8 +1,9 @@
-import { getRandomInterviewCover } from '@/public/utils';
+import { getRandomInterviewCover } from '@/lib/utils';
 import dayjs from 'dayjs';
 import Image from 'next/image';
 import { Button } from './ui/button';
 import Link from 'next/link';
+import DisplayTechIcons from './DisplayTechIcons';
 
 const InterviewCard = ({
   interviewId,
@@ -59,7 +60,7 @@ const InterviewCard = ({
         </div>
 
         <div className="flex flex-row justify-between">
-          <p>Tech Icons</p>
+          <DisplayTechIcons techStack={techstack} />
 
           <Button className="btn-primary">
             <Link
