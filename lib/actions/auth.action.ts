a7f194f721b5216f5296c52e63e1f730630ae408
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-30 22:10:19 -0700

    working on auth actions

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,10 +1,25 @@
 'user server';
 
+import { db } from '@/components/firebase/admin';
+
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
 
   try {
-  } catch (error) {
-    console.error('Error creating a user', error);
+    const userRecord = await db.collection('users').doc(uid).get();
+  } catch (e: any) {
+    console.error('Error creating a user', e);
+
+    if (e.code === 'auth/email-already-exists') {
+      return {
+        success: false,
+        message: 'This email already in use.',
+      };
+    }
+
+    return {
+      success: false,
+      message: 'Failed to create and account.',
+    };
   }
 }
